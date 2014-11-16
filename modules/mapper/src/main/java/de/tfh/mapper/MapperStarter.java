feee56d47bf7bbd55347d34ed780e329645f7c53
w.glanzer <wernerglanzer@googlemail.com>
2014-11-16 11:07:33 +0100

    Modul: Mapper

---
+++ modules/mapper/src/main/java/de/tfh/mapper/MapperStarter.java
@@ -0,0 +1,16 @@
+package de.tfh.mapper;
+
+/**
+ * Haupteinstiegspunkt des Mappers
+ *
+ * @author W.Glanzer, 16.11.2014
+ */
+public class MapperStarter
+{
+
+  public static void main(String[] args)
+  {
+
+  }
+
+}
